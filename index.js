e9236242ab0087d70e542bb527432a554c1296f1
kevin <minghui.liu@trincoll.edu>
2017-02-25 17:43:43 -0500

    working prototype

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var server = require('http').createServer(app);
 var app = express()
 
 var Twitter = require('twitter');
@@ -9,21 +10,33 @@ var client = new Twitter({
   access_token_secret: 'unhCgl9kPy8jMj0BNqIfOq3PhRFDgvJBp1kWmI8N4vNkG'
 });
  
+// use array to store tweets for noew
+// I know this is really bad
+// will switch to a database later
+//var tweets = [];
+
+var io = require('socket.io')(server);
+io.on('connection', function(){
+    console.log("a socket connection");
+    io.emit('tweet', {'message':"hello"});
+});
+
 // You can also get the stream in a callback if you prefer. 
-client.stream('statuses/filter', {track: 'whitehouse, trump'}, function(stream) {
-  stream.on('data', function(event) {
-    console.log(event && event.text);
+client.stream('statuses/filter', {track: 'trump'}, function(stream) {
+  stream.on('data', function(data) {
+    //console.log(data.text);
+    io.emit('tweet', {'message': data.text});
   });
  
   stream.on('error', function(error) {
-    throw error;
+    throw error; 
   });
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// app.get('/', function (req, res) {
+//   res.render('')
+// })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+server.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
+});
