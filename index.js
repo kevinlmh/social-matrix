696dab3c4fa4139c9c382f81ec344e8cd2d6956b
kevin <minghui.liu@trincoll.edu>
2017-02-25 16:17:43 -0500

    add twitter stream

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var express = require('express')
 var app = express()
 
+var Twitter = require('twitter');
+var client = new Twitter({
+  consumer_key: 'D7FJRxEaDTfsIeINim3LfRh6L',
+  consumer_secret: 'SmUDoSCH1lYZWF5wkGNut0aJ8ze2PE1NDNNhygQuQom9GPJ2Oc',
+  access_token_key: '1069974536-qtR9DEEhCfciHd30EAPQBe9OLFqBeVRl2YO24Ke',
+  access_token_secret: 'unhCgl9kPy8jMj0BNqIfOq3PhRFDgvJBp1kWmI8N4vNkG'
+});
+ 
+// You can also get the stream in a callback if you prefer. 
+client.stream('statuses/filter', {track: 'whitehouse, trump'}, function(stream) {
+  stream.on('data', function(event) {
+    console.log(event && event.text);
+  });
+ 
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
